561959a7299ba82ed0b457b235746202b238f98f
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-21 03:58:04 +0500

    complete pages Order Complete and pickup and destiation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
     },
     {
       title: 'Pickup and Destination',
-      url: '/pickup-and-destinationst',
+      url: 'pickup-and-destination',
       icon: 'bicycle'
     },
     {
